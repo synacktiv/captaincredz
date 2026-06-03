fc026e9c9d6b97a58941fe3445c8c1e122efb369
synacktiv-antoineg <102024014+synacktiv-antoineg@users.noreply.github.com>
2026-06-03 17:38:19 +0200

    Fix aws_sms_notify post_action

--- app/plugin/post_actions/aws_sms_notify/__init__.py
+++ app/plugin/post_actions/aws_sms_notify/__init__.py
@@ -18,5 +18,5 @@ import requests
 def action(username, password, request, plugin, result, action_params=None):
     if type(action_params) is not dict or not "lambda_id" in action_params or not "param_name" in action_params:
         return False
-    url = f"https://{action_params.get("lambda_id")}.lambda-url.eu-west-3.on.aws/"
+    url = f"https://{action_params.get('lambda_id')}.lambda-url.eu-west-3.on.aws/"
     requests.get(url, params={action_params.get("param_name"): 'Hit on spray'}, proxies={'http': action_params.get("proxy"), 'https': action_params.get("proxy")})
